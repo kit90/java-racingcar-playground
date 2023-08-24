7d7316bf274aa3e109a058959c3c15f2e25cb5e0
kitcat09 <jk65333@gmail.com>
2023-08-25 00:10:47 +0900

    feat: 자동차 이름 중복 검증
    자동차 이름은 중복될 수 없다는 요구사항, 기능, 테스트 추가

--- src/main/java/racing/Cars.java
+++ src/main/java/racing/Cars.java
@@ -1,32 +1,80 @@
 package racing;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 
 public class Cars {
-    private List<Car> carList;
+    private final List<Car> racingCars;
 
     public Cars(String names) {
-        this.carList = makeCarList(names);
+        this.racingCars = makeCarList(names);
     }
 
     public static Cars makeCarsByNames(String names) {
         return new Cars(names);
     }
 
-    private List<Car> makeCarList(String names) {
-       return splitName(names).stream()
+    private List<Car> makeCarList(String inputNames) {
+        List<String> names = splitNames(inputNames);
+        validateDuplication(names);
+        return names.stream()
                 .map(Car::makeCarByName)
                 .collect(Collectors.toList());
     }
 
-    private List<String> splitName(String names) {
-        return Arrays.asList(names.split(","));
+    private List<String> splitNames(String inputNames) {
+        return Arrays.asList(inputNames.split(","));
     }
 
-    public List<Car> getCarList() {
-        return carList;
+    private void validateDuplication(List<String> inputList) {
+        boolean duplicated = inputList.stream()
+                .distinct()
+                .count() != inputList.size();
+        if(duplicated) {
+            throw new RuntimeException("중복된 자동차 이름은 입력할 수 없습니다.");
+        }
     }
+
+    public void setRacingCarsSpeed(List<Integer> speeds) {
+        IntStream.range(0, racingCars.size())
+                        .forEach(i -> {
+                            Car car = racingCars.get(i);
+                            car.setCarSpeed(speeds.get(i));
+                        });
+    }
+
+    public void forwardRacingCars() {
+        racingCars.stream().forEach(Car::forwardCar);
+    }
+
+    public List<Car> getMaxForwardCars() {
+        int maxForwardCount = getMaxForwardCount();
+        return racingCars.stream()
+                .filter(car -> car.getForwardCount() == maxForwardCount)
+                .collect(Collectors.toList());
+    }
+
+    private int getMaxForwardCount() {
+        return racingCars.stream()
+                .mapToInt(Car::getForwardCount)
+                .max()
+                .orElse(0);
+    }
+
+    public Car findByCarName(String name) {
+        return racingCars.stream()
+                .filter(car -> car.equalsName(name))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("이름에 해당하는 자동차가 없습니다."));
+    }
+
+    public List<Car> getRacingCars() {
+        return racingCars;
+    }
+
 }
---
+++ src/test/java/racing/CarsTest.java
@@ -0,0 +1,112 @@
+package racing;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class CarsTest {
+    @DisplayName("입력한_자동차_이름들로_cars_생성하고_이름들이_맞는지")
+    @Test
+    void create_cars_by_names(){
+        //given
+        Cars cars = Cars.makeCarsByNames("pobi,crong,honux");
+        //when
+        List<String> nameList =  cars.getRacingCars()
+                .stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
+        //then
+        assertThat(nameList).contains("pobi", "crong", "honux");
+    }
+
+    @DisplayName("자동차_이름은_5자를_초과하면_예외_발생")
+    @Test
+    void cars_names_length() {
+        String errMsg = String.format("자동차 이름은 %d자를 초과할 수 없습니다.", Car.MAX_NAME_LENGTH);
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(()->{
+                    Cars.makeCarsByNames("pobi, pobicrong");
+                }).withMessageMatching(errMsg);
+    }
+
+    @DisplayName("자동차_이름은_중복될_수_없다")
+    @Test
+    void cars_names_duplication() {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(()->{
+                    Cars.makeCarsByNames("pobi, pobi");
+                }).withMessageMatching("중복된 자동차 이름은 입력할 수 없습니다.");
+    }
+
+    @DisplayName("자동차_속도는_0~9_사이_벗어나면_예외_발생")
+    @ParameterizedTest
+    @MethodSource("speedRangeArgs")
+    void cars_speeds_range(List<Integer> integers) {
+        Cars cars = Cars.makeCarsByNames("pobi,crong,honux");
+
+        String errMsg = String.format("자동차 속도는 %d~%d사이여야합니다.", CarSpeed.MIN_SPEED, CarSpeed.MAX_SPEED);
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(()->{
+                    cars.setRacingCarsSpeed(integers);
+                }).withMessageMatching(errMsg);
+    }
+
+    static Stream<Arguments> speedRangeArgs() {
+        return Stream.of(
+                Arguments.arguments(Arrays.asList(-1, 2, 3)),
+                Arguments.arguments(Arrays.asList(1, -1, 3)),
+                Arguments.arguments(Arrays.asList(2, 0, -1)),
+                Arguments.arguments(Arrays.asList(-1, -1, -1)),
+                Arguments.arguments(Arrays.asList(10, 9, 5)),
+                Arguments.arguments(Arrays.asList(9, 10, 5)),
+                Arguments.arguments(Arrays.asList(0, 9, 10))
+        );
+    }
+
+    @DisplayName("속도가_4이상일_때_1전진")
+    @Test
+    void forward() {
+        //given
+        Cars cars = Cars.makeCarsByNames("pobi,crong,honux");
+        cars.setRacingCarsSpeed(Arrays.asList(5, 4, 3));
+        //when
+        cars.forwardRacingCars();
+        List<Car> racingCars = cars.getRacingCars();
+        //then
+        assertAll("speed 4 or more is 1 forward",
+                () -> assertThat(racingCars.get(0).getForwardCount()).isEqualTo(1),
+                () -> assertThat(racingCars.get(1).getForwardCount()).isEqualTo(1),
+                () -> assertThat(racingCars.get(2).getForwardCount()).isEqualTo(0)
+        );
+    }
+
+    @DisplayName("최대_전진수를_지닌_자동차")
+    @Test
+    void winners() {
+        //given
+        Cars cars = Cars.makeCarsByNames("pobi,crong,honux");
+        //when
+        cars.setRacingCarsSpeed(Arrays.asList(5, 4, 3));
+        cars.forwardRacingCars();
+        cars.setRacingCarsSpeed(Arrays.asList(4, 5, 3));
+        cars.forwardRacingCars();
+        //then
+        List<Car> winners = cars.getMaxForwardCars();
+        List<String> names = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
+        assertThat(names).contains("pobi","crong");
+    }
+}
